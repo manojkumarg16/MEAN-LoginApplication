5bb68fedf71170ae29ff9f71b0e76234f965d5a0
sjana66 <sudip.jana@wipro.com>
2017-08-18 13:17:27 +0530

    UI changes

--- client/src/app/landing-page/landing-page.component.ts
+++ client/src/app/landing-page/landing-page.component.ts
@@ -21,10 +21,12 @@ logout() {
 openNav() {
     document.getElementById("mySidenav").style.width = "250px";
     document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
 }
 
 closeNav() {
     document.getElementById("mySidenav").style.width = "0";
     document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("content").style.marginLeft = "0px";
 }
 }
